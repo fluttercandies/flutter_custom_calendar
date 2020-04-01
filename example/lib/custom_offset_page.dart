46a68af34c5e46766e38ec74378282cb80b76a43
msidolphin <msidolphin@outlook.com>
2020-04-01 13:05:45 +0800

    Update custom_offset_page.dart

--- example/lib/custom_offset_page.dart
+++ example/lib/custom_offset_page.dart
@@ -116,9 +116,6 @@ class CustomStyleDayWidget extends BaseCustomDayWidget {
 
   @override
   void drawNormal(DateModel dateModel, Canvas canvas, Size size) {
-    if (!dateModel.isCurrentMonth) {
-      return;
-    }
     bool isWeekend = dateModel.isWeekend;
     bool isInRange = dateModel.isInRange;
 
@@ -155,9 +152,6 @@ class CustomStyleDayWidget extends BaseCustomDayWidget {
 
   @override
   void drawSelected(DateModel dateModel, Canvas canvas, Size size) {
-    if (!dateModel.isCurrentMonth) {
-      return;
-    }
     //绘制背景
     Paint backGroundPaint = new Paint()
       ..color = Colors.blue
